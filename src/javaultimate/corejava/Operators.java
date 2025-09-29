ff6ad4c07399ee22b064173e3376cf1d546541a2
Lalit <lalitgupta1qq@gmail.com>
2025-09-29 11:36:05 +0530

    operator and user input

--- src/javaultimate/corejava/Operators.java
+++ src/javaultimate/corejava/Operators.java
@@ -1,4 +1,22 @@
 package corejava;
 
 public class Operators {
+    public static void main(String[] args) {
+        System.out.println(4.8%1.1);
+        char c = 'a';
+        String str = "Lalit";
+        System.out.println(c+str);
+
+        if (str instanceof String) {
+            System.out.println("Datatype is String");
+
+            int d = 7;
+            System.out.println(++d*8);
+            System.out.println(d);
+
+            char bd = 't';
+            System.out.println(bd++);
+            System.out.println(bd);
+        }
+    }
 }
--- src/javaultimate/corejava/UserInput.java
+++ src/javaultimate/corejava/UserInput.java
@@ -1,4 +1,24 @@
 package corejava;
 
+import java.util.Scanner;
+
 public class UserInput {
+    public static void main(String[] args) {
+        System.out.println("It works!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first the number:");
+        int a = sc.nextInt();
+
+        System.out.print("Enter the second number:");
+        int b = sc.nextInt();
+
+        System.out.println("Total sum of : " +(a+b));
+
+        boolean ab = sc.hasNextInt();
+        System.out.println(ab);
+
+        String str="Hello Lalait";
+        System.out.println(str);
+    }
 }
