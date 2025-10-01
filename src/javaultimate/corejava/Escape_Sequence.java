cd85aac771b0a25ea207e4775c773847b85eadfb
Lalit <lalitgupta1qq@gmail.com>
2025-10-01 23:29:15 +0530

    escape seguence

---
+++ src/javaultimate/corejava/Escape_Sequence.java
@@ -0,0 +1,11 @@
+package corejava;
+
+public class Escape_Sequence {
+    public static void main(String[] args) {
+        System.out.println("sdfgshdfuer\ngfysdhccvjyr");
+        System.out.println("sdfgshdfuerg\tfysdhccvjyr");
+        System.out.println("sdfgshdfuerg\\fysdhccvjyr");
+        System.out.println("sdfgshdfuerg\'fysdhccvjyr");
+        System.out.println("sdfgshdfuerg\bfysdhccvjyr");
+    }
+}
