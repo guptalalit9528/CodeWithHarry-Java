61c911b922b048c9f84513c48d44a89bb40fd232
Lalit <lalitgupta1qq@gmail.com>
2025-10-01 23:22:17 +0530

    String

---
+++ src/javaultimate/corejava/Str.java
@@ -0,0 +1,10 @@
+package corejava;
+
+public class Str {
+    public static void main(String[] args) {
+
+       String name = new String("lalit");
+       // String naam = "Lalit";
+        System.out.println(name);
+    }
+}
---
+++ src/javaultimate/corejava/Str_Method.java
@@ -0,0 +1,26 @@
+package corejava;
+
+import java.sql.SQLOutput;
+
+public class Str_Method {
+    public static void main(String[] args) {
+        String name = "LaLit";
+        System.out.println(name.length());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.toUpperCase());
+        System.out.println(name.trim());
+        System.out.println(name.substring(1));
+        System.out.println(name.substring(1,3));
+        System.out.println(name.replace('i','o'));
+        System.out.println(name.replace("i","orp"));
+        System.out.println(name.startsWith("La"));
+        System.out.println(name.endsWith("if"));
+        System.out.println(name.charAt(3));
+        System.out.println(name.indexOf("L"));
+        System.out.println(name.indexOf("L",1));
+        System.out.println(name.lastIndexOf("r"));
+        System.out.println(name.equals("lalit"));
+        System.out.println(name.equalsIgnoreCase("lalit"));
+        System.out.println(name.compareTo("LaLit"));
+    }
+}
