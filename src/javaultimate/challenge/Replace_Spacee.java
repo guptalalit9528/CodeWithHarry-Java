6446ed19841548b33adc713bf93bcda3dce2b0d6
Lalit <lalitgupta1qq@gmail.com>
2025-10-01 23:53:19 +0530

    practice set 3

---
+++ src/javaultimate/challenge/Replace_Spacee.java
@@ -0,0 +1,25 @@
+package challenge;
+
+import java.util.Locale;
+
+public class Replace_Spacee {
+    public static void main(String[] args) {
+//        String name = "LaLit Gupta";
+//        System.out.println(name.toLowerCase());
+
+//        System.out.println(name.replace(" ","_"));
+
+
+//        String letter = "Dear <|name|>, Thanks a lot!";
+//        System.out.println(letter.replace("<|name|>","Lalit"));
+
+
+//        String mystring = "This string contains doupble  and triple spaces";
+//        System.out.println(mystring.indexOf("  "));
+//        System.out.println(mystring.indexOf("   "));
+
+
+        System.out.println("Dear Lalit ,\n\t This java Course is Nice.\n\t Thanks");
+
+    }
+}
