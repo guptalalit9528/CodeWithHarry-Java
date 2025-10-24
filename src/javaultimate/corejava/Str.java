fc0eae5d67780d854ac11210edf526758499c395
Lalit <lalitgupta1qq@gmail.com>
2025-10-25 00:09:48 +0530

    String Demo

--- src/javaultimate/corejava/Str.java
+++ src/javaultimate/corejava/Str.java
@@ -3,8 +3,11 @@ package corejava;
 public class Str {
     public static void main(String[] args) {
 
-       String name = new String("lalit");
-       // String naam = "Lalit";
+       //String name = new String("lalit");
+        String name = "Lalit";
+        name = name + " Kumar";
+        System.out.println(name==name);
         System.out.println(name);
+
     }
 }
