cc12f4808592278e9b33da779d0a89398a5f58be
Lalit <lalitgupta1qq@gmail.com>
2025-10-25 00:10:38 +0530

    If Else statement

---
+++ src/javaultimate/corejava/If_Else.java
@@ -0,0 +1,13 @@
+package corejava;
+
+public class If_Else {
+    public static void main(String[] args) {
+        int age = 28;
+        if(age>18){
+            System.out.println("yes boy you can drive");
+        }
+        else{
+            System.out.println("No boy you cannot drive yet!");
+        }
+    }
+}
