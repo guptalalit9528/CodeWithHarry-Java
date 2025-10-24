8a1fb6442d145cf50f1fdb95cc0c27cc1e636bed
Lalit <lalitgupta1qq@gmail.com>
2025-10-25 00:10:04 +0530

    String Method

--- src/javaultimate/corejava/Str_Method.java
+++ src/javaultimate/corejava/Str_Method.java
@@ -5,6 +5,7 @@ import java.sql.SQLOutput;
 public class Str_Method {
     public static void main(String[] args) {
         String name = "LaLit";
+        String last = "Gupta";
         System.out.println(name.length());
         System.out.println(name.toLowerCase());
         System.out.println(name.toUpperCase());
