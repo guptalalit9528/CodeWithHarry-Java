b33bde93eaeccfd615c2881806d21561fb6745c9
Lalit <lalitgupta1qq@gmail.com>
2025-10-25 21:11:19 +0530

    For Loop Used

---
+++ src/javaultimate/corejava/ForLoop.java
@@ -0,0 +1,14 @@
+package corejava;
+
+public class ForLoop {
+    public static void main(String[] args) {
+//        for(int i = 1; i <= 100; i++){
+//            System.out.println(2*i+1);
+//        }
+
+        int n = 5;
+        for(int i = n; i >= 1; i--){
+            System.out.println(i);
+        }
+    }
+}
