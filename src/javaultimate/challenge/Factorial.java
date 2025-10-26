ac88c9132f381e8d78db48a6b14319f5597b18ad
Lalit <lalitgupta1qq@gmail.com>
2025-10-26 23:25:28 +0530

    Factorial

---
+++ src/javaultimate/challenge/Factorial.java
@@ -0,0 +1,16 @@
+package challenge;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number : ");
+        int num = sc.nextInt();
+        int factorial = 1;
+        for(int i = 1; i <= num; i++){
+             factorial = factorial*i;
+        }
+        System.out.println(factorial);
+    }
+}
