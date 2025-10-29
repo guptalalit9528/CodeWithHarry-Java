0b4712ac083ecce931d76c17cf48c4db92208668
Lalit <lalitgupta1qq@gmail.com>
2025-10-29 23:17:15 +0530

    method challenge 2

---
+++ src/javaultimate/challenge/Method_Challenge_2.java
@@ -0,0 +1,35 @@
+package challenge;
+
+import static challenge.MethodChallenge.multiplication;
+
+public class Method_Challenge_2 {
+    public static void main(String[] args) {
+        //problem 1
+//        multiplication(7);
+
+        //problem 2
+//        MethodChallenge.pattern1(4);
+
+        //problem 3
+//        int a = MethodChallenge.sumRect(4);
+//        System.out.println(a);
+
+        //problem 4
+//        MethodChallenge pattern = new MethodChallenge();
+//        pattern.pattern2(4);
+
+        //problem 5
+//        int c = MethodChallenge.fibornaric(2);
+//        System.out.println(c);
+
+        //problem 7
+//        MethodChallenge.pattern2Rec(3);
+
+
+        //problem 8
+//        MethodChallenge.pattern1Rec(5);
+
+        //problem 9
+        MethodChallenge.fahrenheit(42);
+    }
+}
