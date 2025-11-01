f1424a0362a5928e324e32ec364a62d888c824d3
Lalit <lalitgupta1qq@gmail.com>
2025-11-01 23:15:36 +0530

    Reverse Array Using Java Code

---
+++ src/javaultimate/challenge/ReverseArrayUsingJavaCode.java
@@ -0,0 +1,18 @@
+package challenge;
+
+public class ReverseArrayUsingJavaCode {
+    public static void main(String[] args) {
+
+        int [] arr = {1,2,4,5,6};
+        int l = arr.length;
+        int n = Math.floorDiv(l ,2);
+        for(int i = 0; i < n; i++){
+            int temp = arr[i];
+            arr[i] = arr[l-i-1];
+            arr[l-i-1] = temp;
+            }
+        for(int element : arr) {
+            System.out.print(element + " ");
+        }
+    }
+}
