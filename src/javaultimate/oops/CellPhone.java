b6f6bc1561be8a49c96cca6461411b218649f10a
Lalit <lalitgupta1qq@gmail.com>
2025-11-03 23:51:42 +0530

    Cell Phone

---
+++ src/javaultimate/oops/CellPhone.java
@@ -0,0 +1,18 @@
+package oops;
+
+public interface CellPhone {
+    void keypad();
+    void coupuon();
+}
+interface Wifi{
+    void network();
+    void bluetooth();
+}
+interface MediaPlayer{
+    void music();
+    void record();
+}
+interface Camera{
+    void frontCamera();
+    void selfiCamera();
+}
