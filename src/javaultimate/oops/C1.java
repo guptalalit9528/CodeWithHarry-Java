c7095c4548b344cb3691829b14a45f6197c65d48
Lalit <lalitgupta1qq@gmail.com>
2025-11-03 23:50:20 +0530

    C1 Acces Modifier Main

---
+++ src/javaultimate/oops/C1.java
@@ -0,0 +1,19 @@
+package oops;
+
+public class C1{
+    public int x = 5;
+    protected int y = 45;
+    int z = 6;
+    private int a  = 78;
+
+    //same class use all access
+    public void met1(){
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(a);
+    }
+}
+
+
+
