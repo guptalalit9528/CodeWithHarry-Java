068210aeb971f752dd94de0c685f8657518bf2f5
Lalit <lalitgupta1qq@gmail.com>
2025-11-04 23:30:18 +0530

    creating a package

---
+++ src/javaultimate/oops/CreatingAPackage.java
@@ -0,0 +1,15 @@
+package oops;
+
+ //import java.util.Scanner;
+
+//import java.util.*;
+
+public class CreatingAPackage {
+    public static void main(String[] args) {
+
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        System.out.println("Entered : ");
+        int a = sc.nextInt();
+        System.out.println("show : " +a);
+    }
+}
