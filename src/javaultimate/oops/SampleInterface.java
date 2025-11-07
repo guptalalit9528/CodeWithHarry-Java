43eb43001e93d5cddfddb62d03918921a370903d
Lalit <lalitgupta1qq@gmail.com>
2025-11-07 22:59:24 +0530

    Sample Interface

---
+++ src/javaultimate/oops/SampleInterface.java
@@ -0,0 +1,12 @@
+package oops;
+
+public interface SampleInterface {
+    void met1();
+    void met2();
+
+}
+interface ChildSampleInterface extends SampleInterface{
+    void met3();
+    void met4();
+
+}
