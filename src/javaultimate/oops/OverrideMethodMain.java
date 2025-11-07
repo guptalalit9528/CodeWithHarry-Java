c52127e5659fe4ac063e61ac7cb6b0d7b4a518dd
Lalit <lalitgupta1qq@gmail.com>
2025-11-07 22:58:19 +0530

    override method main

---
+++ src/javaultimate/oops/OverrideMethodMain.java
@@ -0,0 +1,12 @@
+package oops;
+
+public class OverrideMethodMain {
+    public static void main(String[] args) {
+//        OverrideMethod ovm = new OverrideMethod();
+//        ovm.method2();
+
+        Overridem ov = new Overridem();
+        //ov.method1();
+        ov.method2();
+    }
+}
